1ae46676653e2402e4801b9e6864b9a6f7bcf5c3
Hou Wen Jie <2258053088@qq.com>
2025-03-31 21:02:15 +0800

    准备模型的训练

--- ultralytics-8.3.2/detect.py
+++ ultralytics-8.3.2/detect.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
 
     # Load a model
     model = YOLO(model=r'D:\Python\graduate_design\ultralytics-8.3.2\yolo11n-seg.pt')
-    model.predict(source=r'D:\Python\graduate_design\ultralytics-8.3.2\football.jpg',
+    model.predict(source=r'D:\Python\graduate_design\ultralytics-8.3.2\panda.jpg',
                   save=True,
                   show=False,
                   imgsz=416,
