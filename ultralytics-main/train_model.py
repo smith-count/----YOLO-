37c459799dcb6809aba692ebcf690385d1d2a6aa
Hou Wen Jie <2258053088@qq.com>
2025-04-01 14:05:18 +0800

    完成了动物模型检测的测试，需要选择合适的样本进行检测。

---
+++ ultralytics-main/train_model.py
@@ -0,0 +1,21 @@
+import warnings
+
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO(r'D:\Python\graduate_design\ultralytics-main\dataset\yolo11.yaml')
+    model.train(data=r'D:\Python\graduate_design\ultralytics-main\dataset\data.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=100,
+                single_cls=False,  # 是否是单类别检测
+                batch=8,
+                close_mosaic=10,
+                workers=0,
+                device='0',
+                optimizer='SGD',
+                amp=True,
+                project='runs/train',
+                name='exp',
+                )
